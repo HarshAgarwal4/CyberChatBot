6262167ff532677cc2108f95f4474b8759955696
HarshAgarwal4 <agarwalh270@gmail.com>
2025-09-20 21:25:52 +0530

    debug

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/api/chat", async (req, res) => {
+    console.log(process.env.systemInstruction)
     try {
         const { query, history } = req.body;
         console.log(history , query)
