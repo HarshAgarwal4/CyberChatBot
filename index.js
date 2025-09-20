af55d24e4c24a4a8fb19bacd996cd709d520d1a9
HarshAgarwal4 <agarwalh270@gmail.com>
2025-09-20 20:42:57 +0530

    chatbot

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { GoogleGenAI } from "@google/genai";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const ai = new GoogleGenAI({
+    apiKey: process.env.GOOGLE_API_KEY,
+});
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.render("chat");
+});
+
+app.post("/api/chat", async (req, res) => {
+    try {
+        const { query, history } = req.body;
+        console.log(history , query)
+        const chat = ai.chats.create({
+            model: "gemini-2.5-flash",
+            // config: {
+            //     systemInstruction: process.env.systemInstruction ,
+            // },
+            history: history,
+        });
+
+        const response1 = await chat.sendMessage({
+            message: query,
+        })
+        res.send({ status: 1, reply: response1.text });
+    } catch (err) {
+        console.error(err);
+        res.send({ status: 0, reply: "Error fetching AI response" });
+    }
+});
+
+app.listen(3000, () => {
+    console.log("server running")
+})
