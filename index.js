83c3f733a3db6fcc79e4a835ef8521aaf67bc687
HarshAgarwal4 <agarwalh270@gmail.com>
2025-09-21 11:04:52 +0530

    document

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { GoogleGenAI } from "@google/genai";
-import path from 'path';
+import path from "path";
 import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 dotenv.config();
@@ -11,41 +11,45 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 const ai = new GoogleGenAI({
-    apiKey: process.env.GOOGLE_API_KEY,
+  apiKey: process.env.GOOGLE_API_KEY,
 });
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.json());
+
+// increase payload size limit
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("chat");
+  res.render("chat");
 });
 
 app.post("/api/chat", async (req, res) => {
-    console.log(process.env.systemInstruction)
-    try {
-        const { query, history } = req.body;
-        console.log(history , query)
-        const chat = ai.chats.create({
-            model: "gemini-2.5-flash",
-            config: {
-                systemInstruction: process.env.systemInstruction ,
-            },
-            history: history,
-        });
+  console.log(process.env.systemInstruction);
+  try {
+    const { query, history } = req.body;
+    console.log(history, query);
 
-        const response1 = await chat.sendMessage({
-            message: query,
-        })
-        res.send({ status: 1, reply: response1.text });
-    } catch (err) {
-        console.error(err);
-        res.send({ status: 0, reply: "Error fetching AI response" });
-    }
+    const chat = ai.chats.create({
+      model: "gemini-2.5-flash",
+      config: {
+        systemInstruction: process.env.systemInstruction,
+      },
+      history: history,
+    });
+
+    const response1 = await chat.sendMessage({
+      message: query,
+    });
+    res.send({ status: 1, reply: response1.text });
+  } catch (err) {
+    console.error(err);
+    res.send({ status: 0, reply: "Error fetching AI response" });
+  }
 });
 
 app.listen(3000, () => {
-    console.log("server running")
-})
+  console.log("server running");
+});
